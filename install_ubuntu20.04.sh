465a9b1a95d5d583b22b5668df4a3f8592623c87
190079 <190079@softwarica.edu.np>
2021-12-04 22:00:56 +0545

    first commit

---
+++ install_ubuntu20.04.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt upgrade
+sudo apt install jenkins -y
+sudo apt install git
+
+
